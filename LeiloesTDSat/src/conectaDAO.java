f031a8f27c8743a7fe8e25b26155fbeac02b60f6
Jonathas dos Santos <jonathas.sf1@gmail.com>
2023-05-14 19:00:46 -0300

    Criação e alteração dos metodos de Conexao e Desconexao da classe de conexao

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -4,30 +4,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/uc11";
+    private final String USER = "root";
+    private final String PASS = "123456789";
+    
+    Connection conn = null;
+    
     public Connection connectDB(){
-        Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexao Realizada");
+        } catch (ClassNotFoundException | SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + ex.getMessage());
         }
         return conn;
     }
     
+    public void disconnectDB(){
+        try{
+            conn.close();
+            System.out.println("Desconectado");
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + ex.getMessage());
+        }
+    
+    }
 }
